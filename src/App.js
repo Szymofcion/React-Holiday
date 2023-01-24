4e8ae58cda52f6ec065446650d2cc3e8170d4253
Szymofcion <Szymon.dziewajs@gmail.com>
2023-01-24 15:26:19 +0100

    added beautifiler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./component/Header";
 import Dreams from "./component/Dreams";
+import Beautifier from './component/UI/Beautifier'
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Dreams />
+      <Beautifier/>
       <Dreams />
     </div>
   );
---
+++ src/component/UI/Beautifier.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+
+import "./Beautifier.scss";
+
+const Beautifier = () => {
+  return (
+    <Fragment>
+      <section class="hero-img section">
+        <p class="hero-img__title">Tropical Paradise</p>
+        <hr />
+        <p class="hero-img__text">Jedyne takie miejsce na ziemi</p>
+        <hr />
+      </section>
+    </Fragment>
+  );
+};
+
+export default Beautifier;
