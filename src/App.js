800106343aae94d0947203d94d74b180130934be
Szymofcion <Szymon.dziewajs@gmail.com>
2023-01-25 16:48:58 +0100

    added react slick

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Dreams from "./component/Dreams";
 import Beautifier from "./component/UI/Beautifier";
 import Island from "./component/Island";
 import Offers from "./component/Offers";
+import AutoPlay from './component/slick/Slick'
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Beautifier />
       <Island />
       <Offers/>
+      <AutoPlay/>
     
     </div>
   );
