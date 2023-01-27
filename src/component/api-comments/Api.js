eabcd4d8a78a850dc864ff1cb1dd2c7a55105aee
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-01-27 11:50:42 +0100

    added generatePerson api

--- src/component/api-comments/Api.js
+++ src/component/api-comments/Api.js
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+
 import "./Api.scss";
 
-const Api = (props) => {
-    
+const Api = () => {
+  const [results, setResults] = useState([]);
+  const generatePerson = async () => {
+    await fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((data) => setResults(data.results));
+  };
+  
+  useEffect(() => {
+    {
+      generatePerson();
+    }
+  }, []);
   return (
     <div className="container">
-      <h3 className="name">{props.name}</h3>
-      <img className="person-img" src={props.img} alt="" />
-      <span className="person-info">tak</span>
+      <h3 className="name">{results[0].name.first}</h3>
+      <img className="person-img" src={results[0].picture.large} alt="" />
+      <span className="person-info">{results[0].location.country}</span>
     </div>
   );
 };
--- src/component/slick/Slick.js
+++ src/component/slick/Slick.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import Api from "../api-comments/Api";
-import Osoba from "../img/Opinie2_1920.jpg";
+
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -20,17 +20,16 @@ export default class AutoPlay extends Component {
       cssEase: "linear",
     };
 
-    const name = "Jakaś Osoba";
-    const img = Osoba;
+
     return (
       <div>
         <h2 className="section-heading">Opinie</h2>
         <Slider {...settings}>
-          <Api name={name} img={Osoba} />
-          <Api name={name} img={Osoba} />
-          <Api name={name} img={Osoba} /> 
-          <Api name={name} img={Osoba} />
-          <Api name={name} img={Osoba} />
+          <Api  />
+          <Api />
+          <Api  /> 
+          <Api />
+          <Api  />
         </Slider>
       </div>
     );
